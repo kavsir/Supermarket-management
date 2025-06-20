a9be090973f468b66e5f451ce87fc173927531f5
Kvasir04 <dinhtruongphong2752004@gmail.com>
2025-06-20 22:28:14 +0700

    tạo MVC

---
+++ Supermarket-management/Supermarket-management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Supermarket_management.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
