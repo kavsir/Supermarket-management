dc4a3e72e46006be7c0f4de4264614ae04a12d02
Vollerei Seele <iamwibu2121@gmail.com>
2025-06-23 22:26:42 +0700

    fff

--- Supermarket-management/Supermarket-management/obj/Debug/net8.0/Supermarket-management.AssemblyInfo.cs
+++ Supermarket-management/Supermarket-management/obj/Debug/net8.0/Supermarket-management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+522f7d4e51730c02231753054958ba1865f87c6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+273b53055dda53bb59c4ca6d7d1b699ecde8d5a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
