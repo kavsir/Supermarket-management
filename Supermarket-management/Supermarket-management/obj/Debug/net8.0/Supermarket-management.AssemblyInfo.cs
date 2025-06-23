fe64311e0482c78070aabb11fc4e1f6236f0c5f9
Hanh <lanhnhat011204@gmail.com>
2025-06-23 16:18:21 +0700

    Đã có 3 giao diện

--- Supermarket-management/Supermarket-management/obj/Debug/net8.0/Supermarket-management.AssemblyInfo.cs
+++ Supermarket-management/Supermarket-management/obj/Debug/net8.0/Supermarket-management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+930c0e668e9e39e2d0bcd7fec171e3f74d889233")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c53b405620c5092e5b6be3c53604bb11734887a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
