7b57a41f9fabb26eb7c2e561b69fdc3d40d68ec5
Vollerei Seele <iamwibu2121@gmail.com>
2025-06-23 22:53:43 +0700

    Lưu lại tính năng admin

--- Supermarket-management/Supermarket-management/obj/Debug/net8.0/Supermarket-management.AssemblyInfo.cs
+++ Supermarket-management/Supermarket-management/obj/Debug/net8.0/Supermarket-management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+273b53055dda53bb59c4ca6d7d1b699ecde8d5a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e46c0fe02ed11aad1aee8297bc7e5c8b6279c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
