930c0e668e9e39e2d0bcd7fec171e3f74d889233
Hanh <lanhnhat011204@gmail.com>
2025-06-23 15:46:11 +0700

    Đã có 3 giao diện user, đơn hàng, giở hàng

---
+++ Supermarket-management/Supermarket-management/Controllers/GioHang.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Supermarket_management.Controllers
+{
+    public class GioHang : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
